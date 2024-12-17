0d5b66067ef6ba543825748c40d789b73938b7dc
maxentr <rouaultmaxent@gmail.com>
2024-12-17 15:31:31 +0100

    feat(web): clear gameCode query param on join error from link

--- apps/web/components/GameLobbyButtons.tsx
+++ apps/web/components/GameLobbyButtons.tsx
@@ -5,6 +5,8 @@ import { FindPublicGameButton } from "@/components/FindPublicGameButton"
 import { JoinGameButton } from "@/components/JoinGameButton"
 import { ReconnectGameButton } from "@/components/ReconnectGameButton"
 import { useSocket } from "@/contexts/SocketContext"
+import { usePathname, useRouter } from "@/i18n/routing"
+import { useSearchParams } from "next/navigation"
 import { useState } from "react"
 type GameLobbyButtonsProps = {
   gameCode?: string
@@ -17,10 +19,24 @@ const GameLobbyButtons = ({
 }: GameLobbyButtonsProps) => {
   const hasGameCode = !!gameCode
   const { getLastGameIfPossible } = useSocket()
+  const router = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
 
   const [loading, setLoading] = useState<boolean>(false)
 
   const lastGame = getLastGameIfPossible()
+
+  const onJoinGameError = () => {
+    let route = `${pathname}`
+    if (searchParams.has("gameCode")) {
+      const nextSearchParams = new URLSearchParams(searchParams.toString())
+      nextSearchParams.delete("gameCode")
+      route += `?${nextSearchParams}`
+    }
+
+    router.replace(route)
+  }
 
   return (
     <div className="flex flex-col gap-2 mt-6">
@@ -33,6 +49,7 @@ const GameLobbyButtons = ({
           loading={loading}
           setLoading={setLoading}
           className="w-full mb-4"
+          onError={onJoinGameError}
         />
       )}
       <FindPublicGameButton
