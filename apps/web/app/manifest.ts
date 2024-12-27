4d92b484d37411d5d7c60264df1530e54b9ba3c0
maxentr <rouaultmaxent@gmail.com>
2024-12-27 13:16:23 +0100

    feat(web): enhance PWA manifest and add service worker

--- apps/web/app/manifest.ts
+++ apps/web/app/manifest.ts
@@ -1,13 +1,33 @@
 import type { MetadataRoute } from "next"
 
-export default function manifest(): MetadataRoute.Manifest {
+type ClientMode =
+  | "auto"
+  | "focus-existing"
+  | "navigate-existing"
+  | "navigate-new"
+
+type Manifest = MetadataRoute.Manifest & {
+  handle_links?: "auto" | "preferred" | "not-preferred"
+  launch_handler?: {
+    client_mode: ClientMode | Array<ClientMode>
+  }
+  iarc_rating_id?: string
+  scope_extensions?: Array<{ origin: string }>
+  edge_side_panel?: {
+    preferred_width: number
+  }
+}
+
+export default function manifest(): Manifest {
   return {
     name: "Skyjo",
     short_name: "Skyjo",
-    description: "Skyjo online PWA",
+    description:
+      "Enjoy the popular card game Skyjo online! Create private games to play with friends, or join public games to challenge players from around the globe.",
     start_url: "/",
     orientation: "portrait",
     display: "standalone",
+    display_override: ["window-controls-overlay"],
     background_color: "#fefdf7",
     theme_color: "#fefdf7",
     icons: [
@@ -22,11 +42,42 @@ export default function manifest(): MetadataRoute.Manifest {
         type: "image/png",
       },
     ],
-    related_applications: [
+    dir: "ltr",
+    lang: "en",
+    categories: ["entertainment", "games", "social"],
+    prefer_related_applications: false,
+    handle_links: "preferred",
+    scope_extensions: [
       {
-        platform: "Skyjo online",
-        url: `${process.env.NEXT_PUBLIC_SITE_URL}/manifest.json`,
+        origin: "https://www.skyjo.online",
       },
     ],
+    launch_handler: {
+      client_mode: "auto",
+    },
+    // TODO: add screenshots for native stores
+    // screenshots: [],
+    edge_side_panel: {
+      preferred_width: 400,
+    },
+    shortcuts: [
+      {
+        name: "Search public games",
+        url: "/search",
+        description: "Search for public games to join",
+      },
+      {
+        name: "Create a private game",
+        url: "/create?private=true",
+        description: "Create a private game to play with friends",
+      },
+      {
+        name: "Create a public game",
+        url: "/create?private=false",
+        description: "Create a public game to play with anyone",
+      },
+    ],
+    // TODO: add iarc rating id
+    // iarc_rating_id:
   }
 }
